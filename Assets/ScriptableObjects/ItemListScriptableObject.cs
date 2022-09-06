0b59c9491fac02f5feb413cee0ad003136fb316d
lowkangn <kangngee62@gmail.com>
2022-09-06 23:38:29 +0800

    Add nouns for Chapter 2-3

--- Assets/ScriptableObjects/ItemListScriptableObject.cs
+++ Assets/ScriptableObjects/ItemListScriptableObject.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 [Serializable]
@@ -9,12 +10,25 @@ public struct Item
     public string singularForm;
     public string secondarySingularForm;
     public string pluralForm;
+    public string secondaryPluralForm;
 
     public bool CheckAnswer(string singular, string plural)
     {
-        return (singular.Equals(singularForm)
-            || (!secondarySingularForm.Equals("") && singular.Equals(secondarySingularForm)))
-            && plural.Equals(pluralForm);
+        bool singularMatches = singular.Equals(singularForm) || singular.Equals(secondarySingularForm);
+        bool hasPluralForm = !pluralForm.Equals("");
+        bool pluralMatches;
+
+        if (hasPluralForm)
+        {
+            pluralMatches = plural.Equals(pluralForm) 
+                || (!secondaryPluralForm.Equals("") && plural.Equals(secondaryPluralForm));
+        }
+        else
+        {
+            pluralMatches = plural.Equals("");
+        }
+
+        return singularMatches && pluralMatches;
     }
 }
 
